62fb5999cadaab9b10be888a3fa016de32896c68
xYokilyoksx <y.deroche@mls.nc>
2019-06-09 14:16:31 +1100

    DattaWriter opérationel

--- src/ReadFile/DataWriter.java
+++ src/ReadFile/DataWriter.java
@@ -2,12 +2,18 @@ package ReadFile;
 
 import Etudiant.Etudiant;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class DataWriter {
 
     public DataWriter(ArrayList<Etudiant> etudiants){
+
+        File file = new File("Data\\exo31modifie.txt");
+        if (file.exists())
+            file.delete();
 
         //Ecrit dans le fichier blop.txt tout les noms
         for (Etudiant etudiant : etudiants) {
